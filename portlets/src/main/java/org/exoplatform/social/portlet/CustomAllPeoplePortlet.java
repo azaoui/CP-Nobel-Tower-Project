3af8f34ee33433bd063ff007a98d2eca43033df3
azaoui <azaoui@exoplatform.com>
2014-10-13 18:43:37 +0200

    add custom people portelt

---
+++ portlets/src/main/java/org/exoplatform/social/portlet/CustomAllPeoplePortlet.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2003-2011 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.social.portlet;
+
+import org.exoplatform.social.webui.connections.UIAllPeople;
+//should be replaced with the custom component child 
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.UIPortletApplication;
+import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+
+@ComponentConfig(
+  lifecycle = UIApplicationLifecycle.class,
+  template = "app:/groovy/social/portlet/CustomAllPeoplePortlet.gtmpl"
+)
+public class CustomAllPeoplePortlet extends UIPortletApplication {
+
+  public CustomAllPeoplePortlet() throws Exception {
+    addChild(UIAllPeople.class, null, null);
+  }
+
+}
